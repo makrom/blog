25f0b9b92fbf725e28f0a270d41d1d99fedeee0f
Roman <makrom@users.noreply.github.com>
2019-08-13 08:56:34 +0300

    Home controller

--- blog_app/config/routes.rb
+++ blog_app/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+#  devise_for :users, controllers: {
+#    sessions: 'users/sessions'
+#  }
+  get 'persons/profile', as: 'user_root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
+  root to: "home#index"
 end
